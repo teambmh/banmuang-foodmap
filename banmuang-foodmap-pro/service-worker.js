ff7bcaa2156025ee5dfc73097adce1e13c61ceb0
teambmh <teambmh@gmail.com>
2026-06-01 11:22:00 +0700

    Delete banmuang-foodmap-pro directory

--- banmuang-foodmap-pro/service-worker.js
+++
@@ -1,4 +0,0 @@
-const CACHE = "banmuang-foodmap-v1";
-const ASSETS = ["./","index.html","css/style.css","js/app.js","manifest.json"];
-self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
